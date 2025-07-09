6c2aa992934e12658f2ed3798ad817868005911d
As55GPT98Cde <quick_humane4a@icloud.com>
2025-07-08 18:05:33 -0700

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME = 'daysync-cache-v1';
-const URLsToCache = ['.', 'index.html', 'manifest.json'];
-
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(URLsToCache))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
